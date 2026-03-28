00b68492cfd3f0ce14efd09e3841fabaf17105d1
fsitter42 <fsitter@42vienna.com>
2026-03-28 11:28:52 +0100

    upload test

---
+++ f_test.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   f_test.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsitter <fsitter@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/28 11:22:06 by fsitter           #+#    #+#             */
+/*   Updated: 2026/03/28 11:23:46 by fsitter          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	*test_calloc(size_t nmemb, size_t size);
+
+void	*test_calloc(size_t nmemb, size_t size)
+{
+	(void)nmemb;
+	(void)size;
+	return (NULL);
+}
